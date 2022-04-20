e7683d0bf601dbd05a6592aa57c09a41055b0c73
XIE <282763367@qq.com>
2022-04-20 18:30:33 +0800

    Initial commit

---
+++ src/key.java
@@ -0,0 +1,9 @@
+import com.sun.javafx.scene.SceneEventDispatcher;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class key {
+    public static void main(String[] args) {
+    }
+}
